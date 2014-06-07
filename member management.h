d7569fe910e7fd4d36bb3d1d83c0d5afda79b3d0
deokhyun <dhggg0410@gmail.com>
2014-06-07 22:51:59 +0900

    programming

--- member management.h
+++ member management.h
@@ -22,6 +22,9 @@ void addMember(DATA *pmember, int memberNum);
 void loadDataFile(DATA *pmember, int memberNum);
 void printMemberInfo(DATA *pmember, int memberNum);
 void saveDataFile(DATA *pmember, int memberNum);
+void deleteMember(DATA *pmember, int memberNum);
+void modifyMember(DATA *pmember, int memberNum);
+void findMember(DATA *pmember, int memberNum);
 void printMainMenu(void);
 void startInterface(void);
 
